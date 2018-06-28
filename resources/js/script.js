aa8a3a09b4941eb9853b5434d273cf7d5952821b
daniyar <zhadyrassyn.daniyar@is.sdu.edu.kz>
2018-06-28 22:50:42 +0600

    adding smooth navigation scrolling

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+    
+    /* for the sticky navigation */
     var waypoint = new Waypoint({
       element: $('.js--section-features'),
       handler: function(direction) {
@@ -10,11 +12,22 @@ $(document).ready(function() {
       },
         offset: 60 
     });
-//    $('.js--section-features').waypoints(function(direction) {
-//        if(direction == 'down') {
-//            $('nav').addClass('sticky');
-//        } else {
-//            $('nav').removeClass('sticky');
-//        }
-//    });
+    
+    /* Scroll on buttons */
+    $('.js--scroll-to-plans').click(function() {
+        $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
+    });
+    
+    $('.js--scroll-to-start').click(function() {
+        $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
+    });
+    
+    /* Navigation scroll */
+    $(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+});
 });
